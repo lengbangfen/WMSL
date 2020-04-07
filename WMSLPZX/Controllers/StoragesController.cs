4723fcd36d95eafc815137cf728f1f956dbc84cc
lbf992 <1960771864@qq.com>
2020-04-07 12:46:31 +0800

    1

---
+++ WMSLPZX/Controllers/StoragesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WMSLPZX.Controllers
+{
+    public class StoragesController : Controller
+    {
+        // GET: Storages
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult loginpage()
+        {
+            return View();
+        }
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
